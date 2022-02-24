d4108d06c9e211ed91e58e6630d86c88b880d6cb
kousum <1135192606@qq.com>
2022-02-24 17:08:01 +0800

    test: Non-function value encountered for default slot. not fixed

--- src/__stories/Button.stories.tsx
+++ src/__stories/Button.stories.tsx
@@ -2,16 +2,19 @@ import {defineComponent, ref, h, StyleValue, Fragment} from 'vue'
 import {storiesOf} from '@storybook/vue3';
 import {Button} from "@kousum/semi-ui-vue";
 import '@kousum/semi-ui-vue/lib/_base/base.css'
-import {IconEdit, IconTick, IconUser} from "@kousum/semi-icons-vue";
+import {IconClose, IconEdit, IconTick, IconUser,IconDelete} from "@kousum/semi-icons-vue";
+import ButtonGroup from "@kousum/semi-ui-vue/lib/button/ButtonGroup";
 
 
 const stories = storiesOf('Button', module);
 
-stories.add('base', () => <Button>123</Button>);
-stories.add('Primary', () => <Button type="primary">UI semi</Button>);
-stories.add('Danger', () => <Button type="danger">hello button</Button>);
+stories.add('Primary', () => <Button type={"primary"}>UI semi</Button>);
+stories.add('theme', () => <Button type={"primary"} theme={"solid"}>UI semi</Button>);
+stories.add('secondary', () => <Button type={"secondary"}>123</Button>);
+stories.add('warning', () => <Button type={"warning"}>123</Button>);
+stories.add('Danger', () => <Button type={"danger"}>hello button</Button>);
 stories.add('Warning', () => <Button type="warning">hello button</Button>);
-stories.add('Danger', () => <>
+stories.add('icon', () => <>
   <Button aria-label='Button demo' icon={<IconTick/>}>hello button</Button>
   <br/>
   <Button aria-label='Button demo' icon={<IconEdit/>} noHorizontalPadding={false}>
@@ -19,3 +22,15 @@ stories.add('Danger', () => <>
   </Button>
   <Button aria-label='Button demo' icon={<IconUser/>}/>
 </>);
+
+stories.add('ButtonGroup', () => <ButtonGroup type={'danger'} theme={'solid'}>
+  <Button icon={<IconEdit />}>
+    编辑
+  </Button>
+  <Button icon={<IconDelete/>}>
+    删除
+  </Button>
+  <Button icon={<IconClose/>}>
+    关闭
+  </Button>
+</ButtonGroup>);
